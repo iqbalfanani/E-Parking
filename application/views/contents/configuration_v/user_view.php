e0bead29a34a3c91cda70434452633b40a1c208e
Iqbal Fanani <iiqbal1809@gmail.com>
2019-09-26 08:25:25 +0700

    tampilan content view mahasiswa, role, user, dashboard, laporan view, monitoring data

---
+++ application/views/contents/configuration_v/user_view.php
@@ -0,0 +1,111 @@
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-2 text-gray-800">User</h1>
+          <!-- DataTales Example -->
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">Data User</h6>
+              <a href="#" class="btn btn-primary btn-icon-split">
+                    <span class="icon text-white-50">
+                      <i class="fas fa-plus"></i>
+                    </span>
+                    <span class="text">Tambah User Baru</span>
+                  </a>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Nama</th>
+                      <th>Alamat</th>
+                      <th>No Telepon</th>
+                      <th>Email</th>
+                      <th>Role</th>                    
+                      <th>Action</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Nama</th>
+                      <th>Alamat</th>
+                      <th>No Telepon</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                      <th>Action</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>Jl. System Architect</td>
+                      <td>081231231</td>
+                      <td>Tiger@gmail.com</td>
+                      <td>Admin</td>
+                      <td><a href="#" class="btn btn-info btn-circle btn-sm">
+                        <i class="fas fa-info-circle"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger btn-circle btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </a></td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Jl. Accountant</td>
+                      <td>08992321</td>
+                      <td>Garrett@gmail.com</td>
+                      <td>User</td>
+                      <td><a href="#" class="btn btn-info btn-circle btn-sm">
+                        <i class="fas fa-info-circle"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger btn-circle btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </a></td>
+                    </tr>
+                    <tr>
+                      <td>Ashton Cox</td>
+                      <td>Jl. Junior Technical Author</td>
+                      <td>082323223</td>
+                      <td>Ashton@gmail.com</td>
+                      <td>User</td>
+                      <td><a href="#" class="btn btn-info btn-circle btn-sm">
+                        <i class="fas fa-info-circle"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger btn-circle btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </a></td>
+                    </tr>
+                    <tr>
+                      <td>Cedric Kelly</td>
+                      <td>Jl. Senior Javascript Developer</td>
+                      <td>0893232423</td>
+                      <td>Cedric</td>
+                      <td>User</td>
+                      <td><a href="#" class="btn btn-info btn-circle btn-sm">
+                        <i class="fas fa-info-circle"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger btn-circle btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </a></td>
+                    </tr>
+                    <tr>
+                      <td>Airi Satou</td>
+                      <td>Jl. Accountant</td>
+                      <td>0894354343</td>
+                      <td>Airi@gmail.com</td>
+                      <td>Admin</td>
+                      <td><a href="#" class="btn btn-info btn-circle btn-sm">
+                        <i class="fas fa-info-circle"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger btn-circle btn-sm">
+                        <i class="fas fa-trash"></i>
+                      </a></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
+        </div>
