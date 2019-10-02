db68bbb556a9ea67b567b7ab585366bda5ed606f
Iqbal Fanani <iiqbal1809@gmail.com>
2019-10-03 04:57:00 +0700

    Backend Login Dan Backend Logout

--- application/controllers/Configuration.php
+++ application/controllers/Configuration.php
@@ -3,43 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Configuration extends CI_Controller {
 
-	public function index()
-	{
-		
+	function __construct(){
+		parent::__construct();
+		if ($this->session->userdata('logged_in')!==TRUE) {
+			redirect('Login');
+		}
+
 	}
 
 	public function Mahasiswa()
 	{
-		$data = [
-			'title' =>"Mahasiswa Card",
-			'contents' => "contents/configuration_v/mahasiswa_view",
-			'data' => array()
-		];
+		if ($this->session->userdata('level')==='1') {
+			$data = [
+				'title' =>"Mahasiswa Card",
+				'contents' => "contents/configuration_v/mahasiswa_view",
+				'data' => array()
+			];
 
-		$this->load->view('layouts/template', $data);
+			$this->load->view('layouts/template', $data);	
+		}else{
+			echo "Access Denied";
+		}
 	}
 
 	public function User()
 	{
-		$data =[
-			'title' =>"User",
-			'contents'=> "contents/configuration_v/user_view",
-			'data'=>array()
-		];
+		if ($this->session->userdata('level')==='1') {
+			$data =[
+				'title' =>"User",
+				'contents'=> "contents/configuration_v/user_view",
+				'data'=>array()
+			];
 
-		$this->load->view('layouts/template', $data);
-	}
-
-	public function Role()
-	{
-		$data=[
-			'title'=>"Role",
-			'contents'=>"contents/configuration_v/role_view",
-			'data'=>array()
-
-		];
-
-		$this->load->view('layouts/template', $data);
+			$this->load->view('layouts/template', $data);
+		}else{
+			echo "Access Denied";
+		}
+		
 	}
 
 }
--- application/views/layouts/template.php
+++ application/views/layouts/template.php
@@ -79,7 +79,7 @@
         <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <a class="btn btn-primary" href="<?php echo site_url('Login/logout') ?>">Logout</a>
         </div>
       </div>
     </div>
@@ -96,14 +96,11 @@
   <script src="<?php echo base_url() ?>assets/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="<?php echo base_url() ?>assets/vendor/chart.js/Chart.min.js"></script>
 
   <script src="<?php echo base_url() ?>assets/vendor/datatables/jquery.dataTables.min.js"></script>
   <script src="<?php echo base_url() ?>assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="<?php echo base_url() ?>assets/js/demo/chart-area-demo.js"></script>
-  <script src="<?php echo base_url() ?>assets/js/demo/chart-pie-demo.js"></script>
 
   <script src="<?php echo base_url() ?>assets/js/demo/datatables-demo.js"></script>
 
