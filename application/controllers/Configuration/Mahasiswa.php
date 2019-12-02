ab4d014443b30f7a5cf02309ed7420ad04c83600
iqbalfanani <iiqbal1809@gmail.com>
2019-12-02 10:28:09 +0700

    Update Mahasiswa dan Postdemo masih error node to mysql

--- application/controllers/Configuration/Mahasiswa.php
+++ application/controllers/Configuration/Mahasiswa.php
@@ -14,12 +14,13 @@ class Mahasiswa extends CI_Controller {
 
 	var $cname = "Configuration/Mahasiswa";
 
-	public function mahasiswa_index()
+	public function index()
 	{
 		if ($this->session->userdata('level')==='1') {
 			$data = [
 				'title' =>"Mahasiswa Card",
 				'headers'=>'config/headers',
+				'cname' => $this->cname,
 				'contents' => "contents/configuration_v/mahasiswa/mahasiswa_view",
 				'footers' => 'config/footers',
 				'data' => array()
@@ -27,12 +28,53 @@ class Mahasiswa extends CI_Controller {
 
 			$this->load->view('layouts/template', $data);	
 		}else{
-			$this->load->view('layouts/403');;
+			$this->load->view('layouts/403');
 		}
 	}
 
 	public function get_list_mahasiswa(){
 		$data['data'] = $this->Mahasiswa_model->get_data_mhs();
+		echo json_encode($data);
+	}
+
+	public function postdemo(){
+		if(isset($_GET['CardID'])) {
+			echo "eko";
+
+
+
+		}
+		else{
+			echo "Empty Card ID";
+			exit();
+		}
+	}
+
+
+	public function form_update_mahasiswa(){
+		$id = $this->input->post('id');
+		$data['data_by']= $this->Mahasiswa_model->get_data_by($id);
+		$this->load->view('contents/configuration_v/mahasiswa/formupdate', $data);
+	}
+
+	public function mhs_update(){
+		$objdata = array(
+			'id_card' => $this->input->post('edit_card'),
+			'nim' => $this->input->post('edit_nim'),
+			'nama_mhs' => $this->input->post('edit_namamhs'),
+			'jurusan' => $this->input->post('edit_jurusan')
+		);
+
+		$id= $this->input->post('id');
+		$data = $this->Mahasiswa_model->update_mhs($objdata, $id);
+
+		echo json_encode($data);
+	}
+
+	public function mhs_delete(){
+		$id = $this->input->post('id');
+
+		$data = $this->Mahasiswa_model->delete_mhs($id);
 		echo json_encode($data);
 	}
 
