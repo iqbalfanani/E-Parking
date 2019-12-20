bb567aa37926111302232bfbca8b667e8330d5f0
iqbalfanani <iiqbal1809@gmail.com>
2019-12-20 23:00:41 +0700

    Update laporan_view.php
    Co-Authored-By: nadyafitrani <nadyafitrani@users.noreply.github.com>
    Co-Authored-By: fairuzafif <fairuzafif@users.noreply.github.com>

--- application/views/contents/general_v/laporan_view.php
+++ application/views/contents/general_v/laporan_view.php
@@ -6,16 +6,39 @@
   <div class="card shadow mb-4">
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary">Data Laporan</h6>
-      <input id="datepicker" width="276" />
+      <div class="card">
+        <div class="card-body row">
+          <div class="col">
+            <h4>Filter Data Laporan</h4>
+            <div class="input-daterange" data-plugin="datepicker">
+              <div class="input-group ">
+                <span class="input-group-addon">
+                  <i class="icon md-calendar" aria-hidden="true"></i>
+                </span>
+                <input id="date1" type="text" class="form-control" name="date1" />
+
+                <span class="input-group-addon">to</span>
+                <div class="input-group col-5">
+                      <input id="date2" type="text" class="form-control" name="date2" />
+                    </div>
+                  </div>
+              </div>
+          </div>        
+        </div>
+        <div class=" card-body col-md-3">
+            <button type="button" class="btn btn-primary btn-icon-split" id="sa" data-toggle="modal" onclick="filter()">
+              <span class="icon text-white-50">
+                <i class="fas fa-filter"></i>
+              </span>
+              <span class="text btn-xs">Filter</span>
+            </button>
+         </div>
+      </div>
     <script>
-        $('#datepicker').datepicker();
+        $('#date1').datepicker();
+        $('#date2').datepicker();
     </script>
-      <button type="button" class="btn btn-primary btn-icon-split" id="sa" data-toggle="modal" data-target="#insertModal">
-        <span class="icon text-white-50">
-          <i class="fas fa-plus"></i>
-        </span>
-        <span class="text">Generate</span>
-      </a>
+      
     </div>
     <div class="card-body">
       <div class="table-responsive">
